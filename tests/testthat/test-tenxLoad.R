3f4f40a6fb38d6db67b7f7c2e1a26092e4d14f53
MVesuviusC <matthewvc1@gmail.com>
2021-05-20 15:46:53 -0400

    First two tests mostly done

--- tests/testthat/test-tenxLoad.R
+++ tests/testthat/test-tenxLoad.R
@@ -2,8 +2,13 @@ test_that("tenx_load_qc works", {
   dir_name <-
     "/home/gdrobertslab/lab/Counts/S0027/outs/filtered_feature_bc_matrix/"
 
-    if(dir.exists(dir_name)) {
-      expect_true(TRUE)
+  if(dir.exists(dir_name)) {
+    seurat_obj <- tenx_load_qc(dir_name, violin_plot = FALSE)
+    
+    test_that("tenx_load_qc loads in data", {
+      expect_true(class(seurat_obj) == "Seurat")
+    })
+    
   } else {
     skip("Required folder not available.")
   }
