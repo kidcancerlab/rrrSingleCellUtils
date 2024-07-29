1c8149ea399540e8a16eb74d8de0152373779402
matt-gust <matt.g0017@gmail.com>
2024-07-29 15:41:51 -0400

    removed attempt at symlink

--- R/velocity_analysis_functions.R
+++ R/velocity_analysis_functions.R
@@ -98,11 +98,8 @@ r_make_loom_files <- function(sobj,
         #get bam path
         bam_path <- bam_paths[[id]]
         #copy bam to local location
-        # tmp_bam_path <- paste0("tmp_bams/", id, ".bam")
-        # system(paste("cp", bam_path, tmp_bam_path))
-
-        #trying to make symlink to bams
-        system(paste0("ln -s ", bam_path, " tmp_bams/", id, ".bam"))
+        tmp_bam_path <- paste0("tmp_bams/", id, ".bam")
+        system(paste("cp", bam_path, tmp_bam_path))
     }
 
     #Make conda environment
@@ -135,7 +132,7 @@ r_make_loom_files <- function(sobj,
                 "placeholder_max_array", as.character(length(unique(samp_ids)) - 1)) #nolint
 
     use_sbatch_template(replace_tibble = replace_tbl,
-                        template = "/home/gdrobertslab/lab/Analysis/MattGust/projects/rrrSingleCellUtils/inst/make_loom_files.sh",
+                        template = paste0(rrrscu, "make_loom_files.sh")
                         submit = TRUE,
                         file_dir = "sbatch/jobs")
 
