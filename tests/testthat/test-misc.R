bf1825d86743bd24dde803c298dbfd4a2c7aa19b
MVesuviusC <matthewvc1@gmail.com>
2024-09-17 08:53:55 -0400

    Renamed test file

--- tests/testthat/test-misc.R
+++
@@ -1,8 +0,0 @@
-test_that("tenx_load_qc works", {
-  if(dir.exists(
-    "/home/gdrobertslab/lab/Counts/S0027/outs/filtered_feature_bc_matrix/")) {
-#    expect_true(is)
-  } else {
-    skip("Required folder not available.")
-  }
-})
